1bf2ba7ec6a08e59a79da2530aa98859eabd1158
yuzuru <yuzuru>
2020-03-27 22:00:08 +0900

    webpack.config.js html-webpack-plugin追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,40 @@
-var debug   = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
-var path    = require('path');
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  entry: "./js/client.js",
+  context: path.join(__dirname, 'src'),
+  entry: './js/client.js',
   module: {
-    rules: [{
-      test: /\.jsx?$/,
+    rules: [
+      {
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
           }
-        }]
-      }]
-    },
-    output: {
-      path: __dirname + "/src/",
-      filename: "client.min.js"
-    },
-    plugins: debug ? [] : [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+        ]
+      }
     ]
+  },
+  output: {
+    path: __dirname + '/src/',
+    filename: 'client.min.js'
+  },
+  plugins: debug
+    ? [
+        new HtmlWebpackPlugin({
+          template: './index.html',
+          filename: 'index.html'
+        })
+      ]
+    : [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+      ]
 };
